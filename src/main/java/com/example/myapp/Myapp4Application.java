c57aa1912b91c47632ed79bd64a30fa163982d64
Leessjj <1229tmdwns@gmail.com>
2025-04-24 12:44:24 +0900

    Initial commit

---
+++ src/main/java/com/example/myapp/Myapp4Application.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp4Application.class, args);
+	}
+
+}
